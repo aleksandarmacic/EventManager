cfe0cf8d021be37e9cba0c2e2d98636b333db485
Aleksandar Macic <aleksandar.macic@apoddo.com>
2024-12-21 16:47:12 +0100

    made changes

--- lib/data/data_sources/event_api.dart
+++ lib/data/data_sources/event_api.dart
@@ -6,13 +6,7 @@ class EventApi {
   Future<List<EventModel>> getEvents() async {
     await Future.delayed(const Duration(seconds: 2));
     final jsonString = await rootBundle.loadString('assets/events.json');
-    print("ovo je json string");
-    print(jsonString);
-    print("***********");
     final List<dynamic> jsonData = json.decode(jsonString);
-    print("ovo je jsoooooon");
-    print(jsonData);
-    print("################");
     return jsonData.map((event) => EventModel.fromJson(event)).toList();
   }
 }
